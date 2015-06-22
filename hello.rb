d2942f1583411ae1c7a6423c878ebc811a688807
Cornet Wong <wong.496@osu.edu>
2015-06-22 16:29:59 +0800

    mesage

--- hello.rb
+++ hello.rb
@@ -1,7 +1,13 @@
 class MyClass
   def hello(name)
-  	puts "Hello #{name}"
+    puts "Hello #{name}"
+  end
+  
+  def bye(name)
+    puts "Bye #{name}"
   end
 end
 
-MyClass.new.hello("Cornet")
+mc = MyClass.new
+mc.hello("Cornet")
+mc.bye("Krzys")
